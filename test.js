7b66fb5b78f3c61a45d6da5624ad46024bc8535c
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-02-05 11:31:51 +0100

    test: extend tests for lightweight version

--- test.js
+++ test.js
@@ -2,27 +2,29 @@
 
 const test = require('ava')
 
-const isHttpUrl = require('./')
+;[{ fn: require('.') }, { fn: require('./lightweight'), isLightweight: true }].forEach(
+  ({ fn: isHttpUrl, isLightweight }) => {
+    test(isLightweight ? 'lightweight » true' : 'true', t => {
+      t.is(isHttpUrl('http://kikobeats.com'), true)
+      t.is(isHttpUrl('https://kikobeats.com'), true)
+      t.is(isHttpUrl('https://www.kikobeats.com'), true)
+      t.is(isHttpUrl('http://www.kikobeats.com'), true)
+      t.is(
+        isHttpUrl(
+          'http://www.ccrscenter.org/sites/default/files/CCRS%20District%20Practices%20Brief.pdf'
+        ),
+        true
+      )
+    })
 
-test('true', t => {
-  t.is(isHttpUrl('http://kikobeats.com'), true)
-  t.is(isHttpUrl('https://kikobeats.com'), true)
-  t.is(isHttpUrl('https://www.kikobeats.com'), true)
-  t.is(isHttpUrl('http://www.kikobeats.com'), true)
-  t.is(
-    isHttpUrl(
-      'http://www.ccrscenter.org/sites/default/files/CCRS%20District%20Practices%20Brief.pdf'
-    ),
-    true
-  )
-})
-
-test('false', t => {
-  t.is(isHttpUrl('http://Http://kikobeats.com'), false)
-  t.is(isHttpUrl('https://. • 3.7M views'), false)
-  t.is(isHttpUrl(), false)
-  t.is(isHttpUrl('callto://'), false)
-  t.is(isHttpUrl('mailto://'), false)
-  t.is(isHttpUrl('httpsucks://lol.wtf'), false)
-  t.is(isHttpUrl('http:!!!\0'), false)
-})
+    test(isLightweight ? 'lightweight » false' : 'false', t => {
+      if (!isLightweight) t.is(isHttpUrl('http://Http://kikobeats.com'), false)
+      t.is(isHttpUrl('https://. • 3.7M views'), false)
+      t.is(isHttpUrl(), false)
+      t.is(isHttpUrl('callto://'), false)
+      t.is(isHttpUrl('mailto://'), false)
+      t.is(isHttpUrl('httpsucks://lol.wtf'), false)
+      if (!isLightweight) t.is(isHttpUrl('http:!!!\0'), false)
+    })
+  }
+)
