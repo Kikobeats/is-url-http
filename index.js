32799d24ea1bcb96f1f4b136bb78692f0c940584
hbkhan <hbkhan@github.com>
2020-06-24 16:32:44 -0500

    Fix Denial of Service Vulnerability

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const URL = global.window ? window.URL : require('url').URL
-const urlRegex = require('url-regex')({ exact: true })
+const urlRegex = require('is-url-superb')
 
 const REGEX_HTTP_PROTOCOL = /^https?:\/\//i
 
 module.exports = url => {
   try {
     const { href } = new URL(url)
-    return REGEX_HTTP_PROTOCOL.test(href) && urlRegex.test(href)
+    return REGEX_HTTP_PROTOCOL.test(href) && urlRegex(href)
   } catch (err) {
     return false
   }
