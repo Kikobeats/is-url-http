cc8d4f8140a73d7af87048e6155781103e0646ab
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-01-09 23:23:24 +0100

    test: don't allow Basic Authentication headers
    it was deprecated in Chromium
    https://bugs.chromium.org/p/chromium/issues/detail?id=82250#c7

--- test.js
+++ test.js
@@ -5,27 +5,27 @@ const test = require('ava')
 ;[{ fn: require('.') }, { fn: require('./lightweight'), isLightweight: true }].forEach(
   ({ fn: isHttpUrl, isLightweight }) => {
     test(isLightweight ? 'lightweight » true' : 'true', t => {
-      t.is(isHttpUrl('http://kikobeats.com'), true)
-      t.is(isHttpUrl("https://en.wikipedia.org/wiki/Amdahl's_law"), true)
-      t.is(isHttpUrl('https://kikobeats.com'), true)
-      t.is(isHttpUrl('https://www.kikobeats.com'), true)
-      t.is(isHttpUrl('http://www.kikobeats.com'), true)
-      t.is(
+      t.true(isHttpUrl('http://kikobeats.com'))
+      t.true(isHttpUrl("https://en.wikipedia.org/wiki/Amdahl's_law"))
+      t.true(isHttpUrl('https://kikobeats.com'))
+      t.true(isHttpUrl('https://www.kikobeats.com'))
+      t.true(isHttpUrl('http://www.kikobeats.com'))
+      t.true(
         isHttpUrl(
           'http://www.ccrscenter.org/sites/default/files/CCRS%20District%20Practices%20Brief.pdf'
-        ),
-        true
+        )
       )
     })
 
     test(isLightweight ? 'lightweight » false' : 'false', t => {
-      if (!isLightweight) t.is(isHttpUrl('http://Http://kikobeats.com'), false)
-      t.is(isHttpUrl('https://. • 3.7M views'), false)
-      t.is(isHttpUrl(), false)
-      t.is(isHttpUrl('callto://'), false)
-      t.is(isHttpUrl('mailto://'), false)
-      t.is(isHttpUrl('httpsucks://lol.wtf'), false)
-      if (!isLightweight) t.is(isHttpUrl('http:!!!\0'), false)
+      if (!isLightweight) t.false(isHttpUrl('http://Http://kikobeats.com'))
+      t.false(isHttpUrl('https://. • 3.7M views'))
+      t.false(isHttpUrl())
+      t.false(isHttpUrl('callto://'))
+      t.false(isHttpUrl('mailto://'))
+      t.false(isHttpUrl('httpsucks://lol.wtf'))
+      if (!isLightweight) t.false(isHttpUrl('https://admin:admin@test-http-login.vercel.app'))
+      if (!isLightweight) t.false(isHttpUrl('http:!!!\0'))
     })
   }
 )
